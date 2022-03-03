eff2f66ee39a8dfe4d60c86bcf4bc3a5751eca5f
Mimi Leon <mireyaleon76@gmail.com>
2022-03-02 18:43:16 -0800

    update meili host

--- config/initializers/meilisearch.rb
+++ config/initializers/meilisearch.rb
@@ -1,4 +1,4 @@
 MeiliSearch::Rails.configuration = {
-  meilisearch_host: 'http://localhost:3000', # example: http://localhost:7700
+  meilisearch_host: 'alt-text-hci-6.herokuapp.com', # example: http://localhost:7700
   meilisearch_api_key: ENV['MEILI_MASTER_KEY']
 }
