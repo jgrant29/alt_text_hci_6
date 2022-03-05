2fad901077092e453e9aa77a3a3f1e48f1d66acc
Mimi Leon <mireyaleon76@gmail.com>
2022-03-05 11:14:41 -0800

    field specification tags

--- app/controllers/alts_controller.rb
+++ app/controllers/alts_controller.rb
@@ -10,7 +10,8 @@ class AltsController < ApplicationController
   # GET /alts or /alts.json
   def index
     search = params[:query] #.present #? ? params[:query] : nil
-    @alts = Alt.search(search, fields: [ :tags,:title])
+    
+    @alts = Alt.search(search, fields: [ :tags_name, :title])
     @alt = Alt.new
   
   
