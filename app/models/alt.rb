117c22aaa00218688a05f7be852f581dea53e05c
Mimi Leon <mireyaleon76@gmail.com>
2022-03-05 12:33:46 -0800

    field specification tags

--- app/models/alt.rb
+++ app/models/alt.rb
@@ -4,7 +4,7 @@ class Alt < ApplicationRecord
   searchkick
 
   acts_as_taggable 
-  scope :search_import, -> { includes(:tags) }
+  scope :search_import, -> { includes(:tag_list) }
 
   has_rich_text :orginal_url
 
@@ -15,7 +15,7 @@ class Alt < ApplicationRecord
   def search_data
   {
     
-    name_tagged: "#{name} #{tags.map(&:name).join(" ")}",
+    name_tagged: taglist,
     title: title
    # body: body
   }
