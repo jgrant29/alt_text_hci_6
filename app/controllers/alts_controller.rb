ec8ffd940e39cf5619fee055394ee011d652ce88
Mimi Leon <mireyaleon76@gmail.com>
2022-03-08 14:51:27 -0800

    field specification tags

--- app/controllers/alts_controller.rb
+++ app/controllers/alts_controller.rb
@@ -11,7 +11,7 @@ class AltsController < ApplicationController
   def index
     search = params[:query] #.present? ? params[:query] : nil
     
-    @alts = Alt.search(search)
+    @alts = Alt.all
    
     @alt = Alt.new
    
