70ad2bfc9e9a162513fd02e53001f0e72c41b1a2
Mimi Leon <mireyaleon76@gmail.com>
2022-03-09 19:17:17 -0800

    open file path

--- app/controllers/alts_controller.rb
+++ app/controllers/alts_controller.rb
@@ -97,17 +97,20 @@ class AltsController < ApplicationController
 
   def is_duplicate
     a = Alt.find_by(id: @alt.id)
-    file1 = URI.parse(a.image.url).open.read
-    #file1 = URI.parse(a.image.url).open
+    file1 = URI.parse(u.image.url).open
     puts file1.class
+    if file1.instance_of?(String)
+      file1 = URI.parse(u.image.url).read
+      puts file1.class
+    end
     img_mod = Phashion::Image.new(file1.path)
     count = 0
     Alt.all.map { |u| 
 
        puts u.title
-       
-       file2 = URI.parse(u.image.url).open.read
-       
+      
+       file2 = URI.parse(u.image.url).open
+    
       
        if img_mod.duplicate?(Phashion::Image.new(file2.path)) == true
           count = count + 1
