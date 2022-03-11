a828d250e7acadf6a2a3accddbea4c65185997fb
Mimi Leon <mireyaleon76@gmail.com>
2022-03-10 16:17:46 -0800

    change default view

---
+++ app/policies/alt_text_policy.rb
@@ -0,0 +1,26 @@
+class AltTextPolicy < ApplicationPolicy
+
+  def new?
+    (user.present? && @record.user_id == user.id) || (user.present? && user.super_user == true)
+  end
+
+  def create?
+    create?
+  end
+
+  def index?
+    create?
+  end
+
+  def show
+    create?
+  end
+
+  def update?
+    create?
+  end
+
+  def destroy?
+    create?
+  end
+end
