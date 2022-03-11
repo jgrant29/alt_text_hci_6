eaf034a89777304407cac26f2a9939e80508a985
Mimi Leon <mireyaleon76@gmail.com>
2022-03-10 21:01:57 -0800

    allow non-users to create/update an alt

--- app/policies/alt_policy.rb
+++ app/policies/alt_policy.rb
@@ -1,7 +1,7 @@
 class AltPolicy < ApplicationPolicy
 
   def new?
-    (user.present? && @record.user_id == user.id) || (user.present? && user.super_user == true) || (user.present? == false)
+    (user.present? && @record.user_id == user.id) || (user.present? && user.super_user == true) 
   end
 
   def create?
