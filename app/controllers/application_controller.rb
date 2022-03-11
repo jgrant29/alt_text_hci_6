f38050a06a69ca7db80f34dd40163439436a177e
Mimi Leon <mireyaleon76@gmail.com>
2022-03-11 14:15:00 -0800

    alt optional user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :set_current_user
 	before_action :configure_permitted_parameters, if: :devise_controller?
 	include Pundit
 
@@ -9,9 +8,7 @@ class ApplicationController < ActionController::Base
     redirect_to root_url, alert: exception.message
   end
 
-  def set_current_user
-    current_user = User.new unless user_signed_in? 
-  end
+  
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
