2c82cdc952b104dada987625933042ad9d7d3992
Mimi Leon <mireyaleon76@gmail.com>
2022-03-12 17:28:12 -0800

    refine search word middle

--- app/models/alt.rb
+++ app/models/alt.rb
@@ -1,7 +1,7 @@
 class Alt < ApplicationRecord
   include ImageUploader::Attachment(:image)
   #include MeiliSearch::Rails
-  searchkick word_start: [:title, :tags, :body]
+  searchkick word_middle: [:title, :tags, :body]
 
   acts_as_taggable 
   scope :search_import, -> { includes(:tags) }
