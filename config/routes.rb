513ab07369f252fffcc310dfadd3b1e458dbb654
Mimi Leon <mireyaleon76@gmail.com>
2022-03-15 06:28:27 -0700

    add vote buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :alt_texts
   resources :alts
+  resources :verifcations do
+    member do
+      patch :verifcation, to 'alts#verification'
+    end
+  end
   devise_for :users
   root "alts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
