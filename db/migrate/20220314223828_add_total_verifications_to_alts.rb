0ad40e63ea0e4533c93b8611d04c974795b4deef
Mimi Leon <mireyaleon76@gmail.com>
2022-03-16 06:10:44 -0700

    change image available sizes

--- db/migrate/20220314223828_add_total_verifications_to_alts.rb
+++ db/migrate/20220314223828_add_total_verifications_to_alts.rb
@@ -1,5 +1,5 @@
 class AddTotalVerificationsToAlts < ActiveRecord::Migration[7.0]
   def change
-    add_column :alts, :total_verifications, :integer, :default => 0
+    add_column :alts, :total_verifications, :integer, default: 0
   end
 end
