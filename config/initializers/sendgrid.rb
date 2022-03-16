98ecb2190aa3bd7073cfc2cf27fc74c37e1f68fb
Mimi Leon <mireyaleon76@gmail.com>
2022-03-15 20:40:26 -0700

    production and dev settings for sendgrid

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -1,11 +1,20 @@
-if Rails.env.development? || Rails.env.production?
+if Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
     ActionMailer::Base.smtp_settings = {
-    user_name:        'kate@seven.army',
-    password:         ENV['GOOGLE_KEY'], #Rails.application.credentials.dig(:google_key),
-    domain:          'gmail.com',
     address:         'smtp.gmail.com',
     port:            '587',
+    user_name:        ENV['GOOGLE_USERNAME'],
+    password:         ENV['GOOGLE_KEY'], #Rails.application.credentials.dig(:google_key),
+    authentication:  :plain,
+    enable_starttls_auto: true
+  }
+elsif Rails.env.development?
+  ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = {
+    address:         'smtp.gmail.com',
+    port:            '587',
+    user_name:        Rails.application.credentials.dig(:google_username),
+    password:         Rails.application.credentials.dig(:google_key),
     authentication:  :plain,
     enable_starttls_auto: true
   }
