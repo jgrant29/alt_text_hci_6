dedcfed4498ea157ba66d817983c844a4b9f8e02
jgrant29 <jmgrant702@gmail.com>
2022-03-15 20:50:12 -0700

    design modifications

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "Alternative Image Project <jmgrant702@gmail.com>"
+  default from: "Seven Army <kate@seven.army>"
   layout "mailer"
 end
