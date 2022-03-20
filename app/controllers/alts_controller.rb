eb656d06e753754d5b81ed12c84880751df929d5
Mimi Leon <mireyaleon76@gmail.com>
2022-03-19 22:22:14 -0700

    Test verify

--- app/controllers/alts_controller.rb
+++ app/controllers/alts_controller.rb
@@ -22,11 +22,9 @@ class AltsController < ApplicationController
       end
     else
       if params[:verified] == "unverified" 
-        @res = Alt.search(search, fields:[:verified => false])
-        @alts = @res.search(search, fields:[:title, :tags, :body], operator: "or")
+        @alts = Alt.search(search, fields:[:title, :tags, :body], operator: "or", where: {verified: false} )
       else
-        @res = Alt.search(search, fields:[:verified => true])
-        @alts = @res.search(search, fields:[:title, :tags, :body], operator: "or")
+        @alts = Alt.search(search, fields:[:title, :tags, :body], operator: "or", where: {verified: true} )
       end
     end
 
