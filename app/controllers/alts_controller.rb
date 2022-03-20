2a763e4c069844403a6a00b13e3ecc039e447bcf
Mimi Leon <mireyaleon76@gmail.com>
2022-03-19 22:09:40 -0700

    only display verified at index

--- app/controllers/alts_controller.rb
+++ app/controllers/alts_controller.rb
@@ -22,9 +22,9 @@ class AltsController < ApplicationController
       end
     else
       if params[:verified] == "unverified" 
-        @alts = Alt.search(search, where: {verified: false}, fields:[:title, :tags, :body], operator: "or")
+        @alts = Alt.search(search, fields:[:title, :tags, :body, :verified => false], operator: "or")
       else
-        @alts = Alt.search(search, where: {verified: true}, fields:[:title, :tags, :body], operator: "or")
+        @alts = Alt.search(search, fields:[:title, :tags, :body, :verified => true], operator: "or")
       end
     end
 
