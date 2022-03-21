03e60b4859feb802ff15b94c2b2d014d53ae213c
Mimi Leon <mireyaleon76@gmail.com>
2022-03-21 14:16:30 -0700

    edit modal overlay opacity

--- app/controllers/alts_controller.rb
+++ app/controllers/alts_controller.rb
@@ -18,7 +18,7 @@ class AltsController < ApplicationController
       end
 
     else
-      if params[:verified] == "unverified" 
+      if params[:verify].present?
         @alts = Alt.search(search, fields:[:title, :tags, :body], operator: "or")
       else
         @alts = Alt.search(search, fields:[:title, :tags, :body], operator: "or")
