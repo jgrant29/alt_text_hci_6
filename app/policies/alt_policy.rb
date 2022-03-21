b85351305e7e9baefb26fa25c295eecfcd0436c9
jgrant29 <jmgrant702@gmail.com>
2022-03-21 10:31:34 -0700

    alt show policy

--- app/policies/alt_policy.rb
+++ app/policies/alt_policy.rb
@@ -4,10 +4,6 @@ class AltPolicy < ApplicationPolicy
     (user.present? && @record.user_id == user.id) || (user.present? && user.super_user == true) 
   end
 
-  def show?
-    new?
-  end
-
   def create?
     create?
   end
