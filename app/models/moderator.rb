13e4b97b7081f6840c0610ec121beee775a426ba
Mimi Leon <mireyaleon76@gmail.com>
2022-03-21 23:28:27 -0700

    update mod policy

--- app/models/moderator.rb
+++ app/models/moderator.rb
@@ -1,14 +1,5 @@
 class Moderator < ApplicationRecord
   belongs_to :user
 
-   searchkick word_middle: [:id, :first_name, :last_name, :email]
-
-  def search_data
-  {
-    id: id,
-    first_name: first_name,
-    last_name: last_name,
-    email: email,
-  }
-  end
+ 
 end
