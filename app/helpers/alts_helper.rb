22d7d0b90ede63e1326c4b0c71a7401e08a39be2
Mimi Leon <mireyaleon76@gmail.com>
2022-04-19 08:15:10 -0700

    join list with commas

--- app/helpers/alts_helper.rb
+++ app/helpers/alts_helper.rb
@@ -3,7 +3,7 @@ module AltsHelper
 		list = alt.tag_list.split(" ").map do |tag_name|
       		link_to tag_name, root_path(tag: tag_name)
     	end
-    	#safe_join list, ", "
+    	safe_join list, ", "
 		
 	end
 
