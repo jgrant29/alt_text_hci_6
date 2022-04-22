b76c58c56dbd379efa0c90d100737fe9922e177c
Mimi Leon <mireyaleon76@gmail.com>
2022-04-21 20:47:42 -0700

    dup check before submit

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
+
+   function previewImage(){
+
+    
+    }
+    var formFile = document.getElementById("formFile")
+    formFile.addEventListener('change', function() {
+       
+        var image_prev = document.getElementById('preview');
+        image_prev.src = URL.createObjectURL(target.files[0]);
+        image_prev.onload = function() {
+        URL.revokeObjectURL(image_prev.src) 
+        }
+    });
+      
+  
+
     function myFunction(x) {
       if (x.matches) { // If media query matches
         document.getElementsByClassName("d-xtra-small");
