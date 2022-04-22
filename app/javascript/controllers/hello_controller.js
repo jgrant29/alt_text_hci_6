75dd0b93ccaf4db603a45c83ca91e07b9da5c57a
Mimi Leon <mireyaleon76@gmail.com>
2022-04-21 22:02:12 -0700

    dup check before submit

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -10,13 +10,13 @@ export default class extends Controller {
 
     function myFunction(x) {
       if (x.matches) { // If media query matches
-        document.getElementsByClassName("col-lg-9");
+        document.body.style.backgroundColor = "red";
       } else {
         document.body.style.backgroundColor = "pink";
       }
     }
 
-    var x = window.matchMedia("(max-width: 700px)")
+    var x = document.getElementsByClassName("col-lg-9");
     xtra_small = document.getElementsByClassName("d-xtra-small");
     myFunction(x) // Call listener function at run time
     x.addListener(myFunction) // Attach listener function on state changes
