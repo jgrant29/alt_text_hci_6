ea689708a8abc31fb6ffd39603ea17d9100cdf74
Mimi Leon <mireyaleon76@gmail.com>
2022-05-03 19:09:15 -0700

    test pixel match buffer

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,7 +4,7 @@ import "./controllers"
 import * as bootstrap from "bootstrap"  
 import "trix"
 import "@rails/actiontext"
-import "pixelmatch"
+import pixelmatch from "pixelmatch"
 
 
 let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))  
