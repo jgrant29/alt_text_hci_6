c3b8c40cc532c2f80c54f94d87940355857ca384
Mimi Leon <mireyaleon76@gmail.com>
2022-05-27 07:39:30 -0700

    add flag_reason to Alt model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_23_142840) do
+ActiveRecord::Schema.define(version: 2022_05_27_141859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2022_03_23_142840) do
     t.text "body"
     t.boolean "flag"
     t.integer "total_verifications", default: 0
+    t.string "flag_reason"
     t.index ["user_id"], name: "index_alts_on_user_id"
   end
 
