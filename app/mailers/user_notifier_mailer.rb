af44d00d96125d5ce0ec77b87d40dbbae81f8516
Mimi Leon <mireyaleon76@gmail.com>
2022-05-31 21:40:15 -0700

    remove assocaiton from user

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -3,7 +3,7 @@ class UserNotifierMailer < ApplicationMailer
 	# send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(user)
     @user = user
-    mail( :to => @user.email,
+    mail( :to => @user.username,
     :subject => 'Thanks for signing up for our amazing app' )
   end
 end
