ea4c9e3df50e2b3b3905f792fdfa4a99cfde4e10
Mimi Leon <mireyaleon76@gmail.com>
2022-06-17 13:37:11 -0700

    search query on home

--- app/controllers/alts_controller.rb
+++ app/controllers/alts_controller.rb
@@ -16,7 +16,8 @@ class AltsController < ApplicationController
     elsif search.nil?
       if params[:tag].nil? == false 
          @alts = Alt.search(params[:tag], fields:[:tags], operator: "or")
-     
+      elsif params[:tag].nil? == true
+        @alts = Alt.search(search, fields:[:title, :tags, :body], operator: "or")
       else 
         @alts = Alt.where(verified: true, flag: false).shuffle.first(3)
       end
