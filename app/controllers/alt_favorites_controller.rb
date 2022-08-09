2475aadc53bd542dab6eecd6c4dfb59943745197
Mimi Leon <mireyaleon76@gmail.com>
2022-08-08 17:50:48 -0700

    rendr notice

--- app/controllers/alt_favorites_controller.rb
+++ app/controllers/alt_favorites_controller.rb
@@ -55,8 +55,8 @@ class AltFavoritesController < ApplicationController
     respond_to do |format|
       session[:return_to] ||= request.referer
       
-      #format.html { redirect_to session.delete(:return_to), notice: "Removed from My Seven Army favorites." }
-      format.html { render :inline => "<i class='fa fa-heart-regular'></i>" }
+      format.html { redirect_to session.delete(:return_to), notice: "Removed from My Seven Army favorites." }
+     // format.html { render :inline => "<i class='fa fa-heart-regular'></i>" }
       format.json { head :no_content }
       format.js
     end
