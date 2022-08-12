7fd0b859303a05f9c808ab7c0f2ad00b788ea3d3
Mimi Leon <mireyaleon76@gmail.com>
2022-08-12 14:10:02 -0700

    delete favorites

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -38,20 +38,21 @@ document.addEventListener("turbo:submit-start", (event) => {
 
 function toggleFav(e){
   console.log(e.currentTarget);
-   e.currentTarget.classList.toggle("fa-heart-o");
-  if (  e.currentTarget.classList == "fa-heart-o"){
-          Rails.ajax({
+  // e.currentTarget.classList.toggle("fa-heart-o");
+
+  /*if (  e.currentTarget.classList == "fa-heart-o"){
+        Rails.ajax({
           type:"POST",
           url:"/alt_favorites",
           dataType:"json",
           success:function(result){
-            alert(result);
+            console.log(result);
           }
         })
        
-  } 
+  } */
 
- // e.currentTarget.classList.toggle("fa-heart-o");
+  e.currentTarget.classList.toggle("fa-heart-o");
  
 }
 
