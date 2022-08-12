a5e9fe10e9f6da5f8b229d9262123031dcfedfba
Mimi Leon <mireyaleon76@gmail.com>
2022-08-12 10:16:36 -0700

    delete favorites

--- app/controllers/alt_favorites_controller.rb
+++ app/controllers/alt_favorites_controller.rb
@@ -60,7 +60,7 @@ class AltFavoritesController < ApplicationController
       
       format.html { redirect_to session.delete(:return_to), notice: "Removed from My Seven Army favorites." }
   
-      format.json  { redirect_to session.delete(:return_to), notice: "Removed from My Seven Army favorites." }#{ head :no_content }
+      #format.json  { redirect_to session.delete(:return_to), notice: "Removed from My Seven Army favorites." }#{ head :no_content }
       
       #format.js
     end
