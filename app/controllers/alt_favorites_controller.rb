98ae2f4d9287776fa45afdd108ee222a596fc58e
Mimi Leon <mireyaleon76@gmail.com>
2022-08-21 07:57:45 -0700

    edit notice

--- app/controllers/alt_favorites_controller.rb
+++ app/controllers/alt_favorites_controller.rb
@@ -58,11 +58,10 @@ class AltFavoritesController < ApplicationController
    
     respond_to do |format|
       session[:return_to] ||= request.referer
-      #, notice: "Removed from My Seven Army favorites." 
       
-      format.html { redirect_to session.delete(:return_to) }
+      format.html { redirect_to session.delete(:return_to), notice: "Removed from My Seven Army favorites." }
   
-      format.json  { redirect_to session.delete(:return_to)}
+      format.json  { redirect_to session.delete(:return_to), notice: "Removed from My Seven Army favorites." }
      
       
     end
