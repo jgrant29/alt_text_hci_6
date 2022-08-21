74e0902feb9f4a84fe3a07ba00e09bc13acf5879
Mimi Leon <mireyaleon76@gmail.com>
2022-08-21 12:46:54 -0700

    edit notice

--- app/controllers/alt_favorites_controller.rb
+++ app/controllers/alt_favorites_controller.rb
@@ -61,7 +61,8 @@ class AltFavoritesController < ApplicationController
       
       format.html { redirect_to session.delete(:return_to), notice: "Removed from My Seven Army favorites." }
   
-      format.json  { redirect_to session.delete(:return_to), notice: "Removed from My Seven Army favorites." }
+      #format.json  { redirect_to session.delete(:return_to), notice: "Removed from My Seven Army favorites." }
+      format.js
      
       
     end
